f8562f30237eb76e8021268c0aa7d831ea2c4874
Jonas Nilsson <jonas.nilsson@esss.se>
2017-02-09 17:28:08 +0100

    Modified ADKafka demo to use the plugin for pushing data back to Kafka.

--- m-epics-ADKafka/startup/ADKafka_demo.cmd
+++ m-epics-ADKafka/startup/ADKafka_demo.cmd
@@ -1,24 +1,27 @@
 require adcore
 require pvaSrv
+require ADPluginKafka
 
 epicsEnvSet("PREFIX", "$(PREFIX=DMSC)")
 epicsEnvSet("KFKDET_PORT", "$(PREFIX)_AD_KAFKA")
 epicsEnvSet("K_PORT", "$(PREFIX)K")
 epicsEnvSet("QSIZE", "20")
 
-KafkaDriverConfigure("$(KFKDET_PORT)", 0, 0, 0, 0, "some_broker", "some_topic")
-dbLoadRecords("ADKafka.template", "P=$(PREFIX):, R=CAM:, PORT=$(KFKDET_PORT), ADDR=0, TIMEOUT=1")
+KafkaDriverConfigure("$(KFKDET_PORT)", 10, 0, 0, 0, "10.4.0.216:9092", "test_topic")
+dbLoadRecords("ADKafka.template", "P=$(PREFIX):, R=KFK_DRVR:, PORT=$(KFKDET_PORT), ADDR=0, TIMEOUT=1")
 
-#KafkaPluginConfigure("$(K_PORT)", 3, 1, "$(SIMDET_PORT)", 0)
-#dbLoadRecords("KafkaPlugin.template", "P=$(PREFIX),R=:KFK:,PORT=$(K_PORT),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(SIMDET_PORT)")
+KafkaPluginConfigure("$(K_PORT)", 3, 1, "$(KFKDET_PORT)", 0, -1, "10.4.0.216:9092", "ad_kafka_plugin_topic")
+dbLoadRecords("ADPluginKafka.template", "P=$(PREFIX):, R=KFK_PLG:,PORT=$(K_PORT),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(KFKDET_PORT)")
 
 iocInit
 
 startPVAServer
 
-#dbpf $(PREFIX):KFK:EnableCallbacks Enable
-#dbpf $(PREFIX):CAM:Acquire 1
+dbpf $(PREFIX):KFK_PLG:EnableCallbacks Enable
+dbpf $(PREFIX):KFK_DRVR:Acquire 1
 #dbpf $(PREFIX):KFK:KafkaBrokers "10.4.0.215:9092"
 #dbpf $(PREFIX):KFK:KafkaBrokerTopic "ad_topic"
+
+#asynSetTraceMask("DMSC_AD_KAFKA", 0, 0xff)
 
 #Remember; file MUST end with a new line
