3671500d626f1fa42187c6af96ed996a1f88ddb4
Tom Trafford <tom.trafford@diamond.ac.uk>
2019-03-21 16:25:21 +0000

    Point startup scripts and ADKafka IOC to point at current kafka broker

--- iocs/ADKafkaIOC/iocBoot/iocADKafka/st.cmd
+++ iocs/ADKafkaIOC/iocBoot/iocADKafka/st.cmd
@@ -11,7 +11,7 @@ epicsEnvSet("QSIZE", "20")
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 
 
-KafkaDriverConfigure("$(KFKDET_PORT)", 10, 0, 0, 0, "10.4.0.216:9092", "test_topic")
+KafkaDriverConfigure("$(KFKDET_PORT)", 10, 0, 0, 0, "cs04r-sc-vserv-197:9092", "sim_data_topic")
 dbLoadRecords("$(ADKAFKA)/db/ADKafka.template", "P=$(PREFIX):, R=KFK_DRVR:, PORT=$(KFKDET_PORT), ADDR=0, TIMEOUT=1")
 
 NDPvaConfigure("PVA", $(QSIZE), 0, "$(KFKDET_PORT)", 0, $(PREFIX):PVA:Image, 0, 0, 0)
--- startup/ADKafka_demo.cmd
+++ startup/ADKafka_demo.cmd
@@ -6,10 +6,10 @@ epicsEnvSet("KFKDET_PORT", "$(PREFIX)_AD_KAFKA")
 epicsEnvSet("K_PORT", "$(PREFIX)K")
 epicsEnvSet("QSIZE", "20")
 
-KafkaDriverConfigure("$(KFKDET_PORT)", 10, 0, 0, 0, "10.4.0.216:9092", "test_topic")
+KafkaDriverConfigure("$(KFKDET_PORT)", 10, 0, 0, 0, "cs04r-sc-vserv-197:9092", "test_topic")
 dbLoadRecords("ADKafka.template", "P=$(PREFIX):, R=KFK_DRVR:, PORT=$(KFKDET_PORT), ADDR=0, TIMEOUT=1")
 
-KafkaPluginConfigure("$(K_PORT)", 3, 1, "$(KFKDET_PORT)", 0, -1, "10.4.0.216:9092", "ad_kafka_plugin_topic")
+KafkaPluginConfigure("$(K_PORT)", 3, 1, "$(KFKDET_PORT)", 0, -1, "cs04r-sc-vserv-197:9092", "sim_data_topic")
 dbLoadRecords("ADPluginKafka.template", "P=$(PREFIX):, R=KFK_PLG:,PORT=$(K_PORT),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(KFKDET_PORT)")
 
 iocInit
