0c4309f09dadec840744a843d13cfc5acb35c1bb
Jonas Nilsson <jonas.nilsson@esss.se>
2020-02-05 16:29:30 +0100

    Minor fixes.

---
+++ ADPluginKafka/startup/ADPluginKafka_demo.cmd
@@ -0,0 +1,23 @@
+require adcore,2.6+
+require adsimdetector,2.4+
+
+epicsEnvSet("PREFIX", "$(PREFIX=DMSC)")
+epicsEnvSet("SIMDET_PORT", "$(PREFIX)SIMDET")
+epicsEnvSet("K_PORT", "$(PREFIX)K")
+epicsEnvSet("XSIZE", "$(XSIZE=200)")
+epicsEnvSet("YSIZE", "$(YSIZE=200)")
+epicsEnvSet("QSIZE", "20")
+
+simDetectorConfig("$(SIMDET_PORT)", $(XSIZE), $(YSIZE), 1, 0, 0)
+dbLoadRecords("simDetector.template", "P=$(PREFIX):, R=CAM:, PORT=$(SIMDET_PORT), ADDR=0, TIMEOUT=1")
+
+KafkaPluginConfigure("$(K_PORT)", 3, 1, "$(SIMDET_PORT)", 0, -1, "10.4.0.216:9092", "test_topic")
+dbLoadRecords("ADPluginKafka.template", "P=$(PREFIX),R=:KFK:,PORT=$(K_PORT),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(SIMDET_PORT)")
+
+iocInit
+
+dbpf $(PREFIX):KFK:EnableCallbacks Enable
+dbpf $(PREFIX):CAM:AcquirePeriod 2
+dbpf $(PREFIX):CAM:Acquire 1
+
+#Remember; file MUST end with a new line
